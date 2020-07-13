7a4bd956a69e847e7fbf656c3181dd725c7c3d32
Matthew Gilligan <mcgilligan97@gmail.com>
2020-07-13 17:59:54 +0100

    Create initial formatter which takes acronyms into account

---
+++ FormatTitle/Format.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace FormatTitle
+{
+    public class Format
+    {
+        public string DoIt(string title, bool preserveAcronyms)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                return string.Empty;
+
+            var newTitle = new StringBuilder(title.Length * 2);
+
+            newTitle.Append(title[0]);
+
+            for (int i = 1; i < title.Length; i++)
+            {
+                if (char.IsUpper(title[i]))
+                    if ((title[i - 1] != ' ' && !char.IsUpper(title[i - 1])) ||
+                        (preserveAcronyms && char.IsUpper(title[i - 1]) &&
+                        i < title.Length - 1 && !char.IsUpper(title[i + 1])))
+                        newTitle.Append(' ');
+                newTitle.Append(title[i]);
+            }
+
+            return newTitle.ToString();
+        }
+    }
+}
---
+++ FormatTitle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FormatTitle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var format = new Format();
+
+            var move = format.DoIt("WhatsTheMove", true);
+            var acc = format.DoIt("USDIsAmericanDollers", true);
+
+            Console.WriteLine(move);
+            Console.WriteLine(acc);
+        }
+    }
+}
