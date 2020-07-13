f69264267fa9379ff9d12af173603b20c8ca27a0
Matthew Gilligan <mcgilligan97@gmail.com>
2020-07-13 18:08:28 +0100

    Account for underscores

--- FormatTitle/Format.cs
+++ FormatTitle/Format.cs
@@ -7,24 +7,28 @@ namespace FormatTitle
     {
         public string DoIt(string title, bool preserveAcronyms)
         {
-            if (string.IsNullOrWhiteSpace(title))
-                return string.Empty;
-
-            var newTitle = new StringBuilder(title.Length * 2);
-
-            newTitle.Append(title[0]);
-
-            for (int i = 1; i < title.Length; i++)
+            if (title.Contains("_"))
             {
-                if (char.IsUpper(title[i]))
-                    if ((title[i - 1] != ' ' && !char.IsUpper(title[i - 1])) ||
-                        (preserveAcronyms && char.IsUpper(title[i - 1]) &&
-                        i < title.Length - 1 && !char.IsUpper(title[i + 1])))
-                        newTitle.Append(' ');
-                newTitle.Append(title[i]);
+                return title.Replace("_", " ");
             }
+            else
+            {
+                var newTitle = new StringBuilder(title.Length * 2);
 
-            return newTitle.ToString();
+                newTitle.Append(title[0]);
+
+                for (int i = 1; i < title.Length; i++)
+                {
+                    if (char.IsUpper(title[i]))
+                        if ((title[i - 1] != ' ' && !char.IsUpper(title[i - 1])) ||
+                            (preserveAcronyms && char.IsUpper(title[i - 1]) &&
+                            i < title.Length - 1 && !char.IsUpper(title[i + 1])))
+                            newTitle.Append(' ');
+                    newTitle.Append(title[i]);
+                }
+
+                return newTitle.ToString();
+            }
         }
     }
 }
--- FormatTitle/Program.cs
+++ FormatTitle/Program.cs
@@ -10,9 +10,14 @@ namespace FormatTitle
 
             var move = format.DoIt("WhatsTheMove", true);
             var acc = format.DoIt("USDIsAmericanDollers", true);
+            var jpy = format.DoIt("JPY_Funding", true);
+            var baby = format.DoIt("Come_On_Baby", true);
 
             Console.WriteLine(move);
             Console.WriteLine(acc);
+            Console.WriteLine(empty);
+            Console.WriteLine(jpy);
+            Console.WriteLine(baby);
         }
     }
 }
